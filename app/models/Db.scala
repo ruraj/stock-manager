4f250befc31339530c46153e98b22f642c607a96
ruraj.joshi@rts.com.np <ruraj.joshi@rts.com.np>
2014-10-18 00:09:54 +0545

    Add SORM entity for Product and parameters

--- app/models/Db.scala
+++ app/models/Db.scala
@@ -7,7 +7,11 @@ import sorm._ //{Entity, Instance}
  * Created by ruraj on 7/21/14.
  */
 object Db extends Instance (
-  entities = Set(Entity[User](), Entity[Dealer](), Entity[Product](), Entity[DealerProduct](), Entity[UserDealer]()),
+  entities = Set(
+    Entity[User](), Entity[Dealer](), Entity[Token](),
+    Entity[Product](), Entity[Feature](), Entity[models.Value](), Entity[FeatureValue](), Entity[Item](),
+    Entity[DealerProduct](), Entity[UserDealer](),
+    Entity[Transaction]()),
   url = "jdbc:h2:file:sms",
   user = "ruraj",
   password = "password",
